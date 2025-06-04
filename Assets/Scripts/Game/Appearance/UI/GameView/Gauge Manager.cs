af64cef72ea963b8433c42d8f248d0f24aefc516
ilslee <portpine@gmail.com>
2025-06-04 09:16:24 +0900

    Revert "Work of Home(Merging)"
    This reverts commit e4b92fd78ed0eb64d76775e34cbca7112778e8c2.

--- Assets/Scripts/Game/Appearance/UI/GameView/Gauge Manager.cs
+++ Assets/Scripts/Game/Appearance/UI/GameView/Gauge Manager.cs
@@ -10,7 +10,9 @@ using System;
 namespace ssm.game.appearance{
     public class GaugeManager : MonoBehaviour
     {
-        // public int characterIndex = 0;
+        public enum GaugeType {None, HP, EP}
+        public GaugeType gaugeType = GaugeType.None;
+        public int characterIndex = 0;
         private int gaugeLength = 10;
         public Color gaugeBGColor; 
         public List<Color> gaugeColor;
@@ -19,51 +21,13 @@ namespace ssm.game.appearance{
         public UIAnimationManager anim;
         // private float gaugeValue = 0f;
         private int maxValue = 99; // exceeding value will display as this number
-        private void Initialize()
-        {
-            //Set Gauge Colors
-            gaugeColor = new List<Color>(10);
-            Color col00 = new Color(1f,     0f,     0f,1f);
-            Color col01 = new Color(1f,     .5f,    0f,1f);
-            Color col02 = new Color(1f,     1f,     0f,1f);
-            Color col03 = new Color(0f,     1f,    0f,1f);
-            Color col04 = new Color(0f,     .6f,    .3f,1f);
-            Color col05 = new Color(0f,     .5f,   1f,1f);
-            Color col06 = new Color(.2f,    .2f,     1f,1f);
-            Color col07 = new Color(.6f,    0f,     1f,1f);
-            Color col08 = new Color(1f,     0f,     1f,1f);
-            Color col09 = new Color(1f,     0f,     .5f,1f);
-            gaugeColor.Add(col00);
-            gaugeColor.Add(col01);
-            gaugeColor.Add(col02);
-            gaugeColor.Add(col03);
-            gaugeColor.Add(col04);
-            gaugeColor.Add(col05);
-            gaugeColor.Add(col06);
-            gaugeColor.Add(col07);
-            gaugeColor.Add(col08);
-            gaugeColor.Add(col09);
-
-            gaugeBGColor = new Color(0f, 0f, 0f, 1f);
-
-            //Put gauge Icons to List
-            gauge = new List<Image>(gaugeLength);
-            for(int i = 0; i<gaugeLength; i++){
-                gauge.Add(this.transform.GetChild(i).GetComponent<Image>());
-            }
-
-            //Set Text 
-            number.text = "00";
-            
-        }
         public void Start(){
-            Initialize();
             UpdateGauge(0f);
             // SetGauge(13f);
         }
         public void UpdateGauge(float v){
-            if(gaugeColor.Count < gaugeLength){
-                Debug.LogError("GaugeManager.SetGauge : Not enough color variation" + gaugeColor.Count);
+            if(gauge.Count < gaugeLength || gaugeColor.Count < gaugeLength){
+                Debug.LogError("GaugeManager.SetGauge : Not enough gauge length or color variation");
                 return;
             }
             float value = MathF.Min(v, maxValue);// 최대 표기 숫자 제한..레이아웃 맞추기 위해
@@ -72,52 +36,44 @@ namespace ssm.game.appearance{
             // Debug.Log("GaugeManager.UpdateGauge : gaugeGroupID is " + gaugeGroupID.ToString() );
             int gaugeFillcount = gaugeValue % 10 == 0 ? 10 : gaugeValue % 10; // 일 단위만 추출(1~10)
             if(gaugeValue <= 0) gaugeFillcount = 0;
-            // Debug.Log("Gauge Group : " + gaugeGroupID + " / Fill count : " + gaugeFillcount);
             //set gauge color
             Color fillColor = gaugeColor[gaugeGroupID];
             fillColor.a = 1f;
             //set gauge bg color
-            Color bgColor = gaugeGroupID -1 > 0 ? gaugeColor[gaugeGroupID-1] : gaugeBGColor;
+            Color bgColor = gaugeGroupID -1 >= 0 ? gaugeColor[gaugeGroupID-1] : gaugeBGColor;
             bgColor.a = .5f;
 
             for(int i = 0; i < gaugeLength; i++) 
-            {                
+            {
                 if(i < gaugeFillcount){
-                    gauge[i].color = fillColor;                    
+                    gauge[i].color = fillColor;
                 }else{
                     gauge[i].color = bgColor;
                 }
             }
-            
-            
+
+            number.text = gaugeValue.ToString();
         }
         
-        public void SetValue(float v = 0f){
+        public void SetValue(){
+            if(characterIndex == 0) {
+                Debug.LogError("GaugeManager.SetValue : No valid Character Index");
+                return;
+            }
             float startValue = float.Parse(number.text);
-            // float endValue = GetValveFromData();
-            float endValue = v;
+            float endValue = GetValveFromData();
             if(startValue == endValue)return;
-            else anim.AddAnimation(new UIAnimationGauge(gameObject.GetComponent<RectTransform>(), startValue, endValue, 10f, anim.acc.fastsmooth1));
-
-            //Set Text
-            if(v < 10){
-            
-                number.text = "0" + v.ToString();
-            }
-            else if (v > maxValue)
-            {
-                number.text = maxValue.ToString();
-            }
-            else
-            {
-                number.text = v.ToString();
-            }
+            else anim.AddAnimation(new UIAnimationGauge(gameObject.GetComponent<RectTransform>(), startValue, endValue, 1f, anim.acc.fastsmooth1));
         }
-        internal virtual float GetValveFromData(){
+        private float GetValveFromData(){
+            switch(gaugeType){
+                case GaugeType.HP:
+                return GameBoard.Instance().FindCharacter(characterIndex).SearchToken(GameTerms.TokenType.HPCurrent).value0;
+                case GaugeType.EP:
+                return GameBoard.Instance().FindCharacter(characterIndex).SearchToken(GameTerms.TokenType.EPCurrent).value0;
+            }
             return 0f;
         }
-        /*
-        // 수치 업데이트는 Info Manager에서 담당하기로 함 250528
         public void ManageGameEvent(string type, float value){
             
             switch(type){
@@ -129,7 +85,6 @@ namespace ssm.game.appearance{
                 break;
             }
         }
-        */
     }
          
 }
